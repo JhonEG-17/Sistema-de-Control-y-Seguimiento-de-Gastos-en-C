fe53c0befbecd5185e7d2b777c96c26b5f99419c
JhonEG-17 <jhonataneg@outlook.com>
2024-01-30 09:32:15 -0600

    v2.0 Se agrega un menu

--- Program.c
+++ Program.c
@@ -7,7 +7,7 @@ int main (){
 	
 	//declaracion de variables
 	
-	int gasolina, litro, litros1, litros2, litros3, litros4, costo, min1, min2, min3, min4, distancia1, distancia2, distancia3, distancia4, cdmxaoaxaca, cdmxamonterrey, cdmxguadalajara, cdmxacancun, kilometroslitro, costolitro, khora, costogastadoruta1, costogastadoruta2, costogastadoruta3, costogastadoruta4, litrosgastadosruta1, litrosgastadosruta2, litrosgastadosruta3, litrosgastadosruta4, tiemporecorridoruta1, tiemporecorridoruta2, tiemporecorridoruta3, tiemporecorridoruta4, ruta;
+	int gasolina, litro, litros1, litros2, litros3, litros4, costo, min1, min2, min3, min4, distancia1, distancia2, distancia3, distancia4, cdmxaoaxaca, cdmxamonterrey, cdmxguadalajara, cdmxacancun, kilometroslitro, costolitro, khora, costogastadoruta1, costogastadoruta2, costogastadoruta3, costogastadoruta4, litrosgastadosruta1, litrosgastadosruta2, litrosgastadosruta3, litrosgastadosruta4, tiemporecorridoruta1, tiemporecorridoruta2, tiemporecorridoruta3, tiemporecorridoruta4, ruta, selec1, selec2, selec3, selec4, selec5, selec6;
 	
 	char nombres[9][30];
 	
@@ -28,7 +28,7 @@ int main (){
 	
 	system("cls");
 	
-	printf("\n Lista de conductores:\n");
+	printf("\n Lista de conductores:\n\n ");
 	
 	for(int i=0; i<4; i++){
 		printf("%s\n ",nombres[i]);
@@ -169,94 +169,334 @@ int main (){
 		costogastadoruta4=litrosgastadosruta4*costolitro;
 		tiemporecorridoruta4=khora/distancia4;
 		
-	//entrega de datos y resultados
+//entrega de datos y resultados
+
+	//MENU DE SELECCION DE INFORMACION POR CADA CONDUCTOR	
 		
-		//informacion conductor 1
+	do	{
 		
-		printf("\n\n INFORMACION DEL VIAJE DEL CONDUCTOR: ");
+		system("cls");
 		
-		printf("%s\n",nombres[0]);
+		printf("\n\n\n SE HAN ANALIZADO LOS DATOS DE LOS SIGUIENTES CONDUCTORES:\n");
 		
-		printf("\n\n RUTA: ");
+		printf("\n	1.-Nombre:");
+
+		printf("%s",nombres[0]);
 		
-		printf("%s\n",nombres[4]);
+		printf(" RUTA: ");
 		
-		printf("\n\n 1.- Total de litros gastados por cada ruta: ");
+		printf("%s",nombres[4]);
 		
-		printf("%i", litrosgastadosruta1);
-	
-		printf("\n\n 2.- Total de dinero gastado en la ruta ");
+		printf("\n	2.-Nombre:");
+
+		printf("%s",nombres[1]);
 		
-		printf("%i", costogastadoruta1);
-	
-		printf("\n\n 3.- Tiempo que se tardo en recorrer la ruta en horas y minutos ");
+		printf(" RUTA: ");
 		
-		printf("%i", tiemporecorridoruta1);
+		printf("%s",nombres[5]);
+
+		printf("\n	3.-Nombre:");
+
+		printf("%s",nombres[2]);
 		
-		//informacion conductor 2
+		printf(" RUTA: ");
 		
-		printf("\n\n INFORMACION DEL VIAJE DEL CONDUCTOR: ");
-	
-		printf("%s\n",nombres[1]);
+		printf("%s",nombres[6]);
+
+		printf("\n	4.-Nombre:");
+
+		printf("%s",nombres[3]);
 		
-		printf("\n\n RUTA: ");
+		printf(" RUTA: ");
 		
-		printf("%s\n",nombres[5]);
+		printf("%s",nombres[7]);
 		
-		printf("\n\n 1.- Total de litros gastados por cada ruta: ");
+		printf("\n\n	5.- SALIR DEL PROGRAMA.");
 		
-		printf("%i", litrosgastadosruta2);
-	
-		printf("\n\n 2.- Total de dinero gastado en la ruta ");
+		printf("\n\n SELECCIONE LA OPCION PARA CONSULTAR LA INFORMACION: ");
 		
-		printf("%i", costogastadoruta2);
-	
-		printf("\n\n 3.- Tiempo que se tardo en recorrer la ruta en horas y minutos ");
+		scanf("%d", &selec1);
 		
-		printf("%i", tiemporecorridoruta2);
+		switch(selec1)	{
+			//informacion conductor 1
+			case 1:
+			    
+			    do	{
+			    
+					system("cls");
+					
+					printf("\n\n\n LOS SIGUIENTES DATOS HAN SIDO ANALIZADOS:\n 1.-DISTANCIA DEL RECORRIDO\n 2.-LITROS QUE GASTO EN EL RECORRIDO\n 3.-TIEMPO QUE LE TOMO REALIZAR EL RECORRIDO\n 4.-CAPITAL GASTADO EN GASOLINA\n 5.-SALIR AL MENU PRINCIPAL\n\n INGRESE EL NUMERO PREASIGNADO PARA CONSULTAR LA INFORMACION DESEADA: ");
+					
+					scanf("%d", &selec2);
+					
+	    			switch(selec2)	{
+					
+	    				case 1:
+	    					
+	    					printf("\n ");
+	    					
+	    					printf("\n \n DISTANCIA DEL RECORRIDO: ");
+	    					
+	    					printf("%i", &distancia1);
+	    					
+	    					printf("\n");
+	    					
+	    					system("pause");
+	    					
+	    				break;
+	    					
+	    				case 2:
+	    					
+	    					printf("\n \n LITROS QUE GASTO EN EL RECORRIDO: ");
+	    					
+	    					printf("%i", litrosgastadosruta1);
+	    					
+	    					printf("\n");
+	    					
+	    					system("pause");
+	    					
+	    			    break;
+	    					
+	    				case 3:
+	    					
+	    					printf("\n \n TIEMPO QUE LE TOMO REALIZAR EL RECORRIDO: ");
+	    					
+	    					printf("%i", tiemporecorridoruta1);
+	    					
+	    					printf("\n");
+	    					
+	    					system("pause");
+	    					
+	    				break;
+	    				
+					    case 4:
+
+							printf("\n \n \n CAPITAL GASTADO EN GASOLINA");
+
+							printf("%i", costogastadoruta1);
+							
+							printf("\n");
+
+							system("pause");
+
+					    break;
+
+						case 5:
+						break;
+	    			}
+				}while(selec2!=5);
+			break;
+			//informacion conductor 2
+			case 2:
+				
+				do	{
+					
+					system("cls");
+					
+					printf("\n\n\n LOS SIGUIENTES DATOS HAN SIDO ANALIZADOS:\n 1.-DISTANCIA DEL RECORRIDO\n 2.-LITROS QUE GASTO EN EL RECORRIDO\n 3.-TIEMPO QUE LE TOMO REALIZAR EL RECORRIDO\n 4.-CAPITAL GASTADO EN GASOLINA\n 5.-SALIR AL MENU PRINCIPAL\n\n INGRESE EL NUMERO PREASIGNADO PARA CONSULTAR LA INFORMACION DESEADA: ");
+					
+					scanf("%d", &selec3);
+					
+	    			switch(selec3)	{
+	    				
+	    				case 1:
+	    					
+	    					printf("\n \n DISTANCIA DEL RECORRIDO: ");
+	    					
+	    					printf("%i", &distancia2);
+	    					
+	    					printf("\n");
+	    					
+	    					system("pause");
+	    					
+	    				break;
+	    					
+	    				case 2:
+	    					
+	    					printf("\n \n LITROS QUE GASTO EN EL RECORRIDO: ");
+	    					
+	    					printf("%i", litrosgastadosruta2);
+	    					
+	    					printf("\n");
+	    					
+	    					system("pause");
+	    					
+	    				break;
+	    				
+	    				case 3:
+	    					
+	    					printf("\n \n TIEMPO QUE LE TOMO REALIZAR EL RECORRIDO: ");
+	    					
+	    					printf("%i", tiemporecorridoruta2);
+	    					
+	    					printf("\n");
+	    					
+	    					system("pause");
+	    					
+	    				break;
+	    					
+	    				case 4:
+
+							printf("\n \n \n CAPITAL GASTADO EN GASOLINA");
+
+							printf("%i", costogastadoruta2);
+							
+							printf("\n");
+
+							system("pause");
+
+	    				break;
+						case 5:
+						break;
+	    			}
+				}while(selec3!=5);
+			break;
+			//informacion conductor 3
+			case 3:
+				
+				do	{
+					
+					system("cls");
+					
+					printf("\n\n\n LOS SIGUIENTES DATOS HAN SIDO ANALIZADOS:\n 1.-DISTANCIA DEL RECORRIDO\n 2.-LITROS QUE GASTO EN EL RECORRIDO\n 3.-TIEMPO QUE LE TOMO REALIZAR EL RECORRIDO\n 4.-CAPITAL GASTADO EN GASOLINA\n 5.-SALIR AL MENU PRINCIPAL\n\n INGRESE EL NUMERO PREASIGNADO PARA CONSULTAR LA INFORMACION DESEADA: ");
+					
+					scanf("%d", &selec4);
+					
+					switch(selec4)	{
+
+	    				case 1:
+	    					
+	    					printf("\n \n DISTANCIA DEL RECORRIDO: ");
+	    					
+	    					printf("%i", &distancia3);
+	    					
+	    					printf("\n");
+	    					
+	    					system("pause");
+	    					
+	    				break;
+	    					
+	    				case 2:
+	    					
+	    					printf("\n \n LITROS QUE GASTO EN EL RECORRIDO: ");
+	    					
+	    					printf("%i", litrosgastadosruta3);
+	    					
+	    					printf("\n");
+	    					
+	    					system("pause");
+	    					
+	    				break;
+	    					
+	    				case 3:
+	    					
+	    					printf("\n \n TIEMPO QUE LE TOMO REALIZAR EL RECORRIDO: ");
+	    				
+	    					printf("%i", tiemporecorridoruta3);
+	    					
+	    					printf("\n");
+	    				
+	    					system("pause");
+	    					
+	    				break;
+	    					
+	    				case 4:
+
+							printf("\n \n \n CAPITAL GASTADO EN GASOLINA");
+
+							printf("%i", costogastadoruta3);
+							
+							printf("\n");
+
+							system("pause");
+
+	    				break;
+	    				
+						case 5:
+						break;
+	    			}
+		        }while(selec4!=5);
+	    	break;
+			//informacion conductor 4  
+	    	case 4:
+	    	 	
+	    		do	{
+					
+					system("cls");
+					
+					printf("\n\n\n LOS SIGUIENTES DATOS HAN SIDO ANALIZADOS:\n 1.-DISTANCIA DEL RECORRIDO\n 2.-LITROS QUE GASTO EN EL RECORRIDO\n 3.-TIEMPO QUE LE TOMO REALIZAR EL RECORRIDO\n 4.-CAPITAL GASTADO EN GASOLINA\n 5.-SALIR AL MENU PRINCIPAL\n\n INGRESE EL NUMERO PREASIGNADO PARA CONSULTAR LA INFORMACION DESEADA: ");
+					
+					scanf("%d", &selec5);
+					
+	    			switch(selec5){
 		
-		//informacion conductor 3
-		
-		printf("\n\n INFORMACION DEL VIAJE DEL CONDUCTOR: ");
-		
-		printf("%s\n",nombres[2]);
-		
-		printf("\n\n RUTA: ");
-		
-		printf("%s\n",nombres[6]);
-		
-		printf("\n\n 1.- Total de litros gastados por cada ruta: ");
-		
-		printf("%i", litrosgastadosruta3);
-	
-		printf("\n\n 2.- Total de dinero gastado en la ruta ");
-		
-		printf("%i", costogastadoruta3);
-	
-		printf("\n\n 3.- Tiempo que se tardo en recorrer la ruta en horas y minutos ");
-		
-		printf("%i", tiemporecorridoruta3);
-		
-		//informacion conductor 4
-		
-		printf("\n\n INFORMACION DEL VIAJE DEL CONDUCTOR: ");
-		
-		printf("%s\n",nombres[3]);
-		
-		printf("\n\n RUTA: ");
-		
-		printf("%s\n",nombres[7]);
-		
-		printf("\n\n 1.- Total de litros gastados por cada ruta: ");
-		
-		printf("%i", litrosgastadosruta4);
-	
-		printf("\n\n 2.- Total de dinero gastado en la ruta ");
-		
-		printf("%i", costogastadoruta4);
-	
-		printf("\n\n 3.- Tiempo que se tardo en recorrer la ruta en horas y minutos ");
-		
-		printf("%i", tiemporecorridoruta4);
-		
+		    			case 1:
+		    				
+		    				printf("\n \n DISTANCIA DEL RECORRIDO: ");
+		    				
+		    				printf("%i", &distancia4);
+		    				
+		    				printf("\n");
+		    				
+		    				system("pause");
+		    				
+		    			break;
+		    				
+		    			case 2:
+		    				
+		    				printf("\n \n LITROS QUE GASTO EN EL RECORRIDO: ");
+		    				
+		    				printf("%i", litrosgastadosruta4);
+		    				
+		    				printf("\n");
+		    				
+		    				system("pause");
+		    				
+		    			break;
+		    				
+		    			case 3:
+		    				
+		    				printf("\n \n TIEMPO QUE LE TOMO REALIZAR EL RECORRIDO: ");
+		    				
+		    				printf("%i", tiemporecorridoruta4);
+		    				
+		    				printf("\n");
+		    				
+		    				system("pause");
+		    				
+		    			break;
+		    				
+		    			case 4:
+
+							printf("\n \n \n CAPITAL GASTADO EN GASOLINA");
+
+							printf("%i", costogastadoruta4);
+							
+							printf("\n");
+
+							system("pause");
+
+		    			break;
+		    			
+		    			case 5:
+		    			break;
+		    		}
+	    		}while(selec5!=5);
+			break;
+		    case 5:
+	    	break;
+	    }
+	}while(selec1!=5);
+
+printf("\n GRACIAS POR UTILIAR ESTE PROGRAMA :D\n");
+
+printf("\n ESTE PROGRAMA FUE CREADO POR EL DESARROLLADOR JHONATAN ESPINAL GARCIA");
+
+printf("\n ");
+
+system("pause");
+return 0;
 }
+
+	
+	
